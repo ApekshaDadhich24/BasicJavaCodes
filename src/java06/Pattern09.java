f155feeccf594784c634ac6c4694be6f74fd116c
ApekshaDadhich24 <apekshadadhich24@gmail.com>
2026-01-10 16:17:18 +0530

    learned how to push on github

---
+++ src/java06/Pattern09.java
@@ -0,0 +1,8 @@
+package java06;
+
+public class Pattern09 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("learning to push to github");
+    }
+}
