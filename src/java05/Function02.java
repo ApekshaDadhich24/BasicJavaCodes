afcb152f4e91578a1943dd3db415fdfc4234da0e
ApekshaDadhich24 <apekshadadhich24@gmail.com>
2026-03-12 08:47:04 +0530

    armstrongs in a range

--- src/java05/Function02.java
+++ src/java05/Function02.java
@@ -11,7 +11,6 @@ public class Function02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter name 1 : ");
-        String name1 = sc.nextLine();
         System.out.println("Enter name 2 : ");
         String name2= sc.nextLine();
         printTwoNames("Apeksha",name2);
